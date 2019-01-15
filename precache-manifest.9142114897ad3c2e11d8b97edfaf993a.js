2e0e7544998442e0aa46ebd946a93f22dc437c15
AIRCOMAI\r.marek <rmarek@aircom.ag>
2019-01-15 14:58:57 +0100

    Updates

---
+++ precache-manifest.9142114897ad3c2e11d8b97edfaf993a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0b51b3f024dab6c0845d",
+    "url": "/MaciekKostki/static/js/runtime~main.0b51b3f0.js"
+  },
+  {
+    "revision": "2ef8f4afe1ef24f40fdf",
+    "url": "/MaciekKostki/static/js/main.2ef8f4af.chunk.js"
+  },
+  {
+    "revision": "f03e8ae863144a61c620",
+    "url": "/MaciekKostki/static/js/1.f03e8ae8.chunk.js"
+  },
+  {
+    "revision": "2ef8f4afe1ef24f40fdf",
+    "url": "/MaciekKostki/static/css/main.354284e6.chunk.css"
+  },
+  {
+    "revision": "b02b16596711d4ea24c7fb0d5ff05b93",
+    "url": "/MaciekKostki/index.html"
+  }
+];
